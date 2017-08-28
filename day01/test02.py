b33b0d379d7765cdf90597d069646a186a9931bc
xinchao.wang <wangxinchao.ok@163.com>
2017-08-28 15:47:22 +0800

    rename

--- day01/test02.py
+++ day01/test02.py
@@ -1,9 +1,10 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 import urllib2
+
 # 使用代理
-proxyHandler=urllib2.ProxyHandler({"http":"163.125.210.7:9797"})
-proxyOpener=urllib2.build_opener(proxyHandler)
+proxyHandler = urllib2.ProxyHandler({"http": "163.125.210.7:9797"})
+proxyOpener = urllib2.build_opener(proxyHandler)
 urllib2.install_opener(proxyOpener)
-request=urllib2.Request("http://www.baidu.com")
-data=urllib2.urlopen(request).read()
+request = urllib2.Request("http://www.baidu.com")
+data = urllib2.urlopen(request).read()
 print(data)

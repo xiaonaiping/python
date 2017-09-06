ad20fbfe375bd28bb4f29f458b0d323ce71c1740
xinchao.wang <wangxinchao.ok@163.com>
2017-09-06 10:32:28 +0800

    加名字

--- day01/test07.py
+++ day01/test07.py
@@ -10,7 +10,7 @@ import time
 def dealCaptcha(captchaData):
     with open("captchaPic.jpg", "wb") as f:
         f.write(captchaData)
-        # text= raw_input("请输入验证码：1111111111111111111111111111111")
+        # text= raw_input("请输入验证码：1111111111111111111111111111111王新超")
         # return text
 
 
